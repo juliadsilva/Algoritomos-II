51a592ddbe55a6c8ccf4e5890e2edacbc415b7fa
Julia Daniele <juliadanielesilva@outlook.com>
2020-03-27 20:52:29 -0300

    Algoritmos de Listas Ligadas

--- listordenacao.cpp
+++ listordenacao.cpp
@@ -13,34 +13,60 @@ int main(){
 	list<int> lista1; //ponterio para lista um
     list<int> lista2; //ponterio para lista dois
     list<int> lista3; //ponterio para lista tres
+    list<int> :: iterator it1; //interator para varrear a lista um
+    list<int> :: iterator it2; //interator para varrear a lista dois
     int x; //variavel auxiliar para leitura e escrita
+    int tamanholista3;
+    int contador = 0; 
+    int contador1 = 0; 
+    int contador2 = 0; 
 
+    cin >> x;
     while(x!=0){
-        cin >> x;
-        if(x == 0 ) break;
         lista1.push_back(x);
-    }
-    x=1;
-    while(x!=0){
         cin >> x;
-        if(x == 0 ) break;
+    }
+
+    cin >> x;
+    while(x!=0){
         lista2.push_back(x);
+        cin >> x;
     }
 
+    tamanholista3 = lista1.size() + lista2.size();
+   
+    it1 = lista1.begin();
+    it2 = lista2.begin();
 
+    while((contador1 < lista1.size()) && (contador2 < lista2.size())){        
 
-    //Ordenando
-    while(!lista1.empty() || !lista2.empty()){
-        if (*lista1.begin() < *lista2.begin()) moveParaLista3(lista3, lista1);
-        else moveParaLista3(lista3, lista2);
+        if (*it1 < *it2){
+            lista3.push_back(*it1);           
+            it1++;                
+            contador1++;
+        }
+        else{
+            lista3.push_back(*it2);
+            it2++; 
+            contador2++;
+        } 
+
     }
 
-    cout<<"Fora do While\n\n\n";
-    if(lista1.empty()){
-        while(!lista2.empty()) moveParaLista3(lista3, lista2);
+    if(contador1 == lista1.size()){
+        while(contador2 < lista2.size()){
+        lista3.push_back(*it2);
+            it2++; 
+            contador2++;
+        }
     }
-    else{
-        while(!lista1.empty()) moveParaLista3(lista3, lista1);
+
+    if(contador2 == lista2.size()){
+        while(contador1 < lista1.size()){
+        lista3.push_back(*it1);
+            it1++; 
+            contador1++;
+        }
     }
 
     while(!lista3.empty()){
